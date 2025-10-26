436b8a1946dbe13a47448851d152be5272c57ddb
Paul Anderson <2762638a@student.gla.ac.uk>
2025-10-26 19:34:35 +0000

    fix: use thread pointer instead of copy, detach thread draft

--- src/devices/threads.c
+++ src/devices/threads.c
@@ -4,10 +4,19 @@
 #include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "../uxn.h"
 #include "threads.h"
 #include "system.h"
+
+#define LOGGING_ENABLED 1  /* Set to 0 to disable all logging */
+
+#if LOGGING_ENABLED
+#define log_printf(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define log_printf(...) ((void)0)
+#endif
 
 /* CMD
 
@@ -50,12 +59,19 @@ typedef struct {
   Uint16 entry_address;
   Uint16 result_value;
   pthread_t thread_id;
+  bool is_in_use;
+  bool is_detached;
+  bool is_finished;
   pthread_mutex_t thread_mutex;
 } ThreadRecord;
 
 #define MAX_THREAD_COUNT 8
 static ThreadRecord thread_records[MAX_THREAD_COUNT];
 pthread_mutex_t thread_record_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void print_thread_id(pthread_t th) {
+  log_printf("thread_handle=%lu\n", (unsigned long)th);
+}
 
 /* built in macro helpers */
 /* read 16 bits from thread memory starting at low_address */
@@ -71,7 +87,10 @@ static void device_set16(Uint16 low_address, Uint16 value) {
 static Uint8 find_first_free_thread_num(void) {
   Uint8 i;
   for (i = 0; i < MAX_THREAD_COUNT; i++) {
-    if (pthread_mutex_trylock(&thread_records[i].thread_mutex) == 0) {
+    /* Non blocking check for free slot because if something has the mutex it must be in use */
+    if (pthread_mutex_trylock(&thread_records[i].thread_mutex) == 0 && !thread_records[i].is_in_use) {
+      thread_records[i].is_in_use = true;
+      pthread_mutex_unlock(&thread_records[i].thread_mutex);
       return i;
     }
   }
@@ -81,19 +100,19 @@ static Uint8 find_first_free_thread_num(void) {
 /* get a thread record by id */
 static ThreadRecord *get_thread_record(Uint16 thread_id) {
   if (thread_id >= MAX_THREAD_COUNT) return NULL;
-  if (!thread_records[thread_id].is_in_use) return NULL;
+  /*if (!thread_records[thread_id].is_in_use) return NULL;*/
   return &thread_records[thread_id];
 }
 
 /* Thread starts evaluating uxn code here */
 static void *worker_thread_entry(void *args) {
-  Uint8 *thread_num = ((Uint8 *)args);
-  ThreadRecord *record = thread_records[*thread_num];
-  fprintf(stderr,"worker_thread_entry: thread_id=%d\n", *thread_num);
+  Uint8 thread_num = (Uint8)(uintptr_t)args;
+  ThreadRecord* record = &thread_records[thread_num];
+  fprintf(stderr,"worker_thread_entry: thread_id=%d\n", thread_num);
   fprintf(stderr,"worker_thread_entry: started\n");
   fprintf(stderr,"worker_thread_entry: entry_address=0x%04x\n", record->entry_address);
   uxn = uxn_global; /* copy global uxn state to thread-local uxn state */
-  printf(stderr,"uxn ram ptr: %p\n", uxn.ram);
+  fprintf(stderr,"uxn ram ptr: %p\n", uxn.ram);
   uxn_eval(record->entry_address);
 
   fprintf(stderr,"worker_thread_entry: top of stack: %d\n", uxn.wst.dat[uxn.wst.ptr - 1]);
@@ -102,19 +121,34 @@ static void *worker_thread_entry(void *args) {
   Uint16 result = device_get16(PORT_RESULT);
   record->result_value = result;
 
-  printf(stderr,"worker_thread_entry: result_value=0x%04x\n", result);
+  fprintf(stderr,"worker_thread_entry: result_value=0x%04x\n", result);
+
+  pthread_mutex_lock(&record->thread_mutex);
+  record->is_finished = true;
+
+  if (record->is_detached) {
+      /* We are detached: free the slot for reuse */
+      record->is_in_use = false;
+      record->is_detached = false;
+      record->is_finished = false;
+  }
+
+  pthread_mutex_unlock(&record->thread_mutex);
+  return NULL;
 }
 
 /* when a CREATE command is received */
 static void handle_create_command(void) {
   fprintf(stderr,"handle_create_command: started\n");
   Uint16 entry_address = device_get16(PORT_PTR_LO);
-  Uint8 thread_id = find_first_free_thread_num();
+  Uint8 thread_id = 0; /*find_first_free_thread_num();*/
+  thread_records[thread_id].entry_address = entry_address;
   fprintf(stderr,"handle_create_command: creating thread_id=%d\n", thread_id);
   fprintf(stderr,"handle_create_command: entry_address=0x%04x\n", entry_address);
   pthread_create(&thread_records[thread_id].thread_handle, NULL,
-               worker_thread_entry, thread_id);
-  fprintf(stderr,"handle_create_command: created thread_handle=%p\n", thread_records[thread_id].thread_handle);
+               worker_thread_entry, (void *)(uintptr_t)thread_id);
+  fprintf(stderr,"handle_create_command: created ");
+  print_thread_id(thread_records[thread_id].thread_handle);
   device_set16(PORT_OUT_THREAD_LO, thread_id);
 }
 
@@ -125,12 +159,42 @@ static void handle_join_command(void) {
   ThreadRecord *record = get_thread_record(target_thread_id);
 
   fprintf(stderr,"handle_join_command: target_thread_id=%d\n", target_thread_id);
-  fprintf(stderr,"handle_join_command: thread_handle=%p\n", record->thread_handle);
+  fprintf(stderr,"handle_join_command: joining ");
+  print_thread_id(record->thread_handle);
   pthread_join(record->thread_handle, NULL);
+  record->is_in_use = false;
+  record->is_detached = false;
+  record->is_finished = false;
+
   device_set16(PORT_RESULT, record->result_value);
 
 
   fprintf(stderr,"handle_join_command: joined with return value=0x%04x\n", record->result_value);
+}
+
+bool detach_thread(int i) {
+    ThreadRecord *record = &thread_records[i];
+    bool result = false;
+
+    pthread_mutex_lock(&record->thread_mutex);
+
+    if (!record->is_in_use)
+        goto unlock;
+
+    record->is_detached = true;
+    pthread_detach(record->thread_id);
+
+    if (record->is_finished) {
+        record->is_in_use = false;
+        record->is_detached = false;
+        record->is_finished = false;
+    }
+
+    result = true;
+
+unlock:
+    pthread_mutex_unlock(&record->thread_mutex);
+    return result;
 }
 
 /* Device write entry */
