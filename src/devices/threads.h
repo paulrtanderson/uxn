1c9948c746cd3ca4e7f3e5cd63e2b2b5d454b462
Paul Anderson <2762638a@student.gla.ac.uk>
2025-11-11 02:37:43 +0000

    fix: put destroy mutex fn in header

--- src/devices/threads.h
+++ src/devices/threads.h
@@ -2,6 +2,7 @@
 #define DEVICES_THREADS_H
 
 void threads_deo(Uint8 addr);
+void destroy_mutexes();
 
 /* exported so worker threads can boot new VMs from the same ROM */
 extern const char *g_rom_path;
